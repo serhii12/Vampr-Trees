91e0d652e611b239e446498f9224f3e687f09c33
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-11-22 17:56:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Vampr-Trees
+
+This app will be able to quickly determine a vampire's lineage relative to any other vampire. This app will need to be able to answer the following questions:
+
+<ol>
+  <li>
+    Who is a vampire's creator?
+  </li>
+
+  <li>
+    How many vampires has a vampire created?
+  </li>
+
+  <li>
+    How many vampires away from the original vampire is a vampire?
+  </li>
+
+  <li>
+    Who is the more senior vampire out of two vampires? (Who is closer to the original vampire)
+  </li>
+
+  <li>
+    Who is the closest common ancestor of two vampires?
+  </li>
+</ol>
---
+++ test/2_numberOfOffspring.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const Vampire = require('../vampire.js');
+
+describe('Vampire', () => {
+  let rootVampire;
+  beforeEach(() => {
+    rootVampire = new Vampire('root');
+  });
+
+  describe('numberOfOffspring', () => {
+    it('should get the correct number of offspring', () => {
+      expect(rootVampire.numberOfOffspring).to.equal(0);
+      rootVampire.addOffspring(new Vampire());
+      expect(rootVampire.numberOfOffspring).to.equal(1);
+      rootVampire.addOffspring(new Vampire());
+      rootVampire.addOffspring(new Vampire());
+      rootVampire.addOffspring(new Vampire());
+      rootVampire.addOffspring(new Vampire());
+      expect(rootVampire.numberOfOffspring).to.equal(5);
+    });
+  });
+});
